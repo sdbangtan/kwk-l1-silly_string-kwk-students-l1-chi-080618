3e606cff36543a77c66e6c2786ff09f1c5004e63
Saumya Malhotra <ideagem1@gmail.com>
2018-08-07 18:12:03 +0000

    Done.

---
+++ ip2.rb
@@ -0,0 +1,12 @@
+def my_fav_things
+  
+  fav_animal = "puppies"
+  fav_place = "Mussorie"
+  fav_artist = "BTS"
+  
+  puts "I love #{fav_animal}!"
+  puts "I love #{fav_place}!"
+  puts "I love #{fav_artist}!"
+end
+
+my_fav_things
--- silly_string.rb
+++ silly_string.rb
@@ -1,13 +1,13 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "cheesecake"
 
-puts "My favorite word is #{your_favorite_word}"
+puts your_favorite_word
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = "#{your_favorite_word}".upcase
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
@@ -15,7 +15,7 @@ puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = "#{phrase}".downcase
 
 puts lowercased_phrase
 
@@ -23,21 +23,23 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #{letter_count} letters in #{big_word}."
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = "#{big_word}" + "wow" + "mom"
 
-new_letter_count = bigger_word.size
+puts bigger_word
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+new_letter_count = bigger_word.length
+
+puts "There are now #{new_letter_count} letters in #{bigger_word}."
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
@@ -46,6 +48,8 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = sentence.gsub("A", "O").gsub("a", "o")
 
 puts sentence
+
+puts your_favorite_word
